3544be10474cb73953bcfcce2ca45cc2176f41ae
utikpuhlik <utikpuhlik@win.com>
2025-09-07 15:10:04 +0200

    fix: api url

--- streamlit_cad_webapp/config/config.py
+++ streamlit_cad_webapp/config/config.py
@@ -7,9 +7,9 @@ env.read_env()
 
 class Settings(BaseSettings):
     app_name: str = "CAD WebApp"
-    api_endpoint_binary: str = "https://api.eucalytics.uk/cv/v1/predict/binary"
-    api_endpoint_url: str = "https://api.eucalytics.uk/cv/v1/predict/url"
-    api_key: str = env.str("API_KEY", "")
+    api_endpoint_binary: str = "https://api.eucalytics.uk/cad-api/cv/v1/predict/binary"
+    api_endpoint_url: str = "https://api.eucalytics.uk/cad-api/cv/v1/predict/url"
+    api_key: str = env.str("API_KEY")
 
 
 settings = Settings()
